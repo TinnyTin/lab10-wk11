caf9d0a608d9f44a3441c8c16b36d01e349316a0
Martin <martinlee5159@gmail.com>
2021-03-26 15:27:52 -0700

    add p5 to app.js

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,16 @@
 // p4
 toDoList = [
-    {todo: "clean my room", status: "incomplete"},
-    {todo: "eat dinner", status: "incomplete"},
-    {todo: "complete this lab", status: "incomplete"}];
+    {todo: "clean my room", status: "complete"},
+    {todo: "eat dinner", status: "complete"},
+    {todo: "complete this lab", status: "started"},
+    {todo: "travel the world", status: "started"}
+]
 
 completedTodos = []
 
 // p5
-
+for (const todo of toDoList) {
+    if (todo.status === "complete") {
+        completedTodos.push(todo)
+    }
+}
